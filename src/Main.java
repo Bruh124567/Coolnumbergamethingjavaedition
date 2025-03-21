f1cdf47ca0a241d24d01ae6cf971ee5b1153f3e0
Bruh124567 <81039620+Bruh124567@users.noreply.github.com>
2025-03-21 19:25:35 +0100

    First commit with code

---
+++ src/Main.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+
+public class Main {
+    public static boolean numGuess(int answer, int num1, int num2) {
+        boolean correct = false;
+        Scanner scanner = new Scanner(System.in);
+        int k = 8;
+        System.out.println("I'm thinking of a number between " + num2 + " and " + num1 + ".");
+        while (k > 0) {
+            System.out.println("Enter your guess:");
+            int guess = scanner.nextInt();
+            if (guess == answer) {
+              correct = true;
+              break;
+            } else if (guess > answer) {
+                k--;
+                System.out.println("Too high! You have " + k + "guesses left.");
+            } else {
+                k--;
+                System.out.println("Too low! You have " + k + "guesses left.");
+            }
+        }
+        return correct;
+    }
+
+
+    public static void tryAgain() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Would you like to play again?\n1. Yes\n2. No");
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            main(null);
+        } else {
+            System.out.println("Goodbye!");
+        }
+    }
+
+    public static int selCustomDifficulty(int num1, int num2) {
+        System.out.println("Generating numbers...");
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        return rand.nextInt(num1 - num2) + num2;
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome!\nEnter your difficulty:\n1.Default(1-100)\n2.Custom");
+        int choice = scanner.nextInt();
+        if (choice == 1){
+            int answer = random.nextInt(101);
+            boolean correct = numGuess(answer, 1, 100);
+            if (correct) {
+                System.out.println("Correct!\nYou Win!");
+                tryAgain();
+            } else {
+                System.out.println("You ran out of tries.\nYou Lose!");
+                tryAgain();
+            }
+        } else if (choice == 2){
+            System.out.println("Custom Difficulty");
+            System.out.println("Enter the max. number to be generated:");
+            int num1 = scanner.nextInt();
+            if (num1 > 0){
+                System.out.println("Enter the min. number to be generated:");
+                int num2 = scanner.nextInt();
+                if (num2 > 0){
+                    int answer = selCustomDifficulty(num1, num2);
+                    boolean correct = numGuess(answer, num1, num2);
+                    if (correct) {
+                        System.out.println("Correct!\nYou Win!");
+                        tryAgain();
+                    } else {
+                        System.out.println("You ran out of tries.\nYou Lose!");
+                        tryAgain();
+                    }
+                } else {
+                    System.out.println("Number must be positive!\nRestarting...");
+                    main(null);
+                }
+            } else {
+                System.out.println("Number must be positive!\nRestarting...");
+                main(null);
+
+
+            }
+        }
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class test {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int lol = rand.nextInt(5,10);
+        System.out.println(lol);
+    }
+}
